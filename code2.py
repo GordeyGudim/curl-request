d2e1f14f2546d472d7b5856bcf32bb48cf66ac00
GordeyGudim <gordey.gudim@mail.ru>
2022-12-05 20:54:37 +0300

    curl request

---
+++ code2.py
@@ -0,0 +1,10 @@
+import requests
+def test_access():
+    headers = {
+        'Accept': 'application/json',
+        'Authorization': 'OAuth y0_AgAAAABmjXc-AAi5VAAAAADVUQoRoQpQsEdvSaWgsGhVPKQoLzOb_BQ',
+        }
+
+    response = requests.get('https://cloud-api.yandex.net/v1/disk/resources/files', headers=headers)
+    return response
+print(test_access())
